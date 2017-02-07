c60f2f49c1787c1fb87efa3396a2a793d0c534d6
Brett Stephen <igadget777@gmail.com>
2017-02-07 14:06:37 -0500

    Commented out and removed redundant code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('html').attr('id', 'pg-top');
   
-  $.validate({
-    form: '#contactForm',
-    modules: 'html5'
-  });
+  // $.validate({
+  //   form: '#contactForm',
+  //   modules: 'html5'
+  // });
   
   $.fn.serializeObject = function() {
     var o = {};
@@ -37,33 +37,15 @@ $(document).ready(function() {
   $(".navbar-brand, .nav-link").on("click", function(event) {
     event.preventDefault();
 
-    var self = $(this);
+    //var self = $(this);
 
-    var href = self.attr('href');
+    //var href = self.attr('href');
     //console.log(href);
     $("body, html").animate({
-      scrollTop: $(href).offset().top+1
+      //scrollTop: $(href).offset().top+1
+      scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 600);    
     
   });
 
-  $(document).scroll(function() {
-    var scroll_top = $(document).scrollTop();
-    var one_top = $('#portfolio').position().top;
-    var two_top = $('#about').position().top;
-    var three_top = $('#contact').offset().top;
-    var nav_link = $(".nav-link");
-
-    if (nav_link.closest('li').hasClass("active")) {
-      $(".nav-link").closest('li').removeClass('active');
-    }
-
-    if (scroll_top > one_top && scroll_top < two_top) {
-      $(".nav-link:eq(0)").closest('li').addClass('active');
-    } else if (scroll_top > two_top && scroll_top < three_top) {
-      $(".nav-link:eq(1)").closest('li').addClass('active');
-    } else if (scroll_top > three_top) {
-      $(".nav-link:eq(2)").closest('li').addClass('active');
-    }
-  });
 });
